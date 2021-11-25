f84e8b454b92fd9cf0ff5d78c07e991cdffc0153
anitarhmwt15 <81134160+anitarhmwt15@users.noreply.github.com>
2021-11-25 22:04:13 +0700

    Add files via upload

---
+++ Prak6/input_user.php
@@ -0,0 +1,18 @@
+<?php
+SESSION_START();
+if ($_POST["captcha_code"] == $_SESSION["captcha_code"]){
+include "koneksi.php";
+$id_user = $_POST['id_user'];
+$nama = $_POST['nama'];
+$email = $_POST['email'];
+$pass = md5($_POST['password']);
+$sql = "INSERT INTO users(id_user,password, nama, email) VALUES ('$id_user', '$pass', '$nama','$email')";
+$query=mysqli_query($con, $sql);
+mysqli_close($con);
+header('location:tampil_user.php');
+}
+else{
+    echo "<center>Login gagal! username & password tidak benar<br>";
+    echo "<a href=form_login.php><b>ULANGILAGI</b></a></center>";
+}
+?>
